7b406ed5b4fcbd19c161ace1a2253372c72e37fb
universalrent547 <sanjaydeepak915@gmail.com>
2025-05-06 15:27:19 +0530

    app.py

--- app.py
+++ app.py
@@ -79,3 +79,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
